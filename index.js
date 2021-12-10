7e03176ace6235065e78db05c7b9a541db887818
a.vinogradov <a.vinogradov@bpcbt.com>
2021-12-10 08:50:31 +0300

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const shell = require('shelljs');
+const _ = require('lodash');
+const fs = require("fs");
+
+console.log('----SCRIPT-START---');
+
+var stdinBuffer = fs.readFileSync(0);
+eval(stdinBuffer.toString())
+
+console.log('----SCRIPT-END---');
